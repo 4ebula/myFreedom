e042e55e48a848bb8e15a4c5797c0590545e6583
4ebula <4ebula@gmail.com>
2021-03-22 19:40:12 +0300

    fix: adjustments to review

--- homework-03/homework-03.js
+++ homework-03/homework-03.js
@@ -1,16 +1,18 @@
+const questionsArr = [
+  {question: 'Number("false") convertion returns number 0', answer: false},
+  {question: 'The value of variable let can\'t be changed after initialization', answer: false},
+  {question: 'Math object has method for square root calculation', answer: true},
+  {question: 'The result of logical operation execution is boolean', answer: false},
+  {question: 'JS is too complicated and can\'t be learned', answer: false}
+];
+
 function isStatementTrue(){
-  let rightAnswers = 0, wrongAnswers = 0;
-  let questionsArr = [
-    {question: 'Number("false") convertion returns number 0', answer: false},
-    {question: 'The value of variable let can\'t be changed after initialization', answer: false},
-    {question: 'Math object has method for square root calculation', answer: true},
-    {question: 'The result of logical operation execution is boolean', answer: false},
-    {question: 'JS is too complicated and can\'t be learned', answer: false}
-  ];
+  let rightAnswers = 0, 
+      wrongAnswers = 0;
     for(let e of questionsArr){
       askQuestion(e.question, e.answer) ? rightAnswers++ : wrongAnswers++;
     }
-    alert(`Rigth anwers: ${rightAnswers}.\nWrong answers: ${wrongAnswers}.`)
+    alert(`Rigth anwers: ${rightAnswers}.\nWrong answers: ${wrongAnswers}.`);
 }
 
 function askQuestion(question, answer){
