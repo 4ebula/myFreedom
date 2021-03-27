ceca373c177653f6d324310b48448fd1fde1f6ca
4ebula <4ebula@gmail.com>
2021-03-27 15:53:15 +0300

    feat: add deepClone function for object copying

--- lesson-05/lesson-05.js
+++ lesson-05/lesson-05.js
@@ -78,3 +78,53 @@ let _obj3 = {
 }
 console.log('ety' in _obj3);
 console.log('key' in _obj3);
+
+console.log('----------------------------------');
+const city = {
+  name: 'Ленинград',
+  population: 5384342,
+  coordinates: {
+    latitude: 59.93,
+    longitude: 30.36
+  }
+};
+
+let cityClone = {};
+
+function cloneDeep(_oldObj, _newObj) {
+  for (let key in _oldObj) {
+    if (typeof _oldObj[key] === 'object') {
+      _newObj[key] = {};
+      cloneDeep(_oldObj[key], _newObj[key]);
+    }
+    else _newObj[key] = _oldObj[key];
+  }
+}
+
+cloneDeep(city, cityClone);
+console.log('city', '\n', city.coordinates.latitude);
+console.log('cityClone', '\n', cityClone.coordinates.latitude);
+
+city.coordinates.latitude = 60;
+
+console.log('city', '\n', city.coordinates.latitude);
+console.log('cityClone', '\n', cityClone.coordinates.latitude);
+
+
+function cloneDeep2(obj) {
+  const objClone = {};
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') objClone[key] = cloneDeep2(obj[key]);
+    else objClone[key] = obj[key];
+  }
+  return objClone;
+}
+console.log('\n\n\n');
+
+let cityClone2 = cloneDeep2(city);
+console.log('city', '\n', city.coordinates.latitude);
+console.log('cityClone2', '\n', cityClone2.coordinates.latitude);
+
+city.coordinates.latitude = 77;
+console.log('city', '\n', city.coordinates.latitude);
+console.log('cityClone2', '\n', cityClone2.coordinates.latitude);
