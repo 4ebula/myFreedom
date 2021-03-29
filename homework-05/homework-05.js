b9c299639ea011fb9472ed6904a721fdb36fd622
4ebula <4ebula@gmail.com>
2021-03-29 12:23:43 +0300

    fix: remove eval() function

--- homework-05/homework-05.js
+++ homework-05/homework-05.js
@@ -1,33 +1,32 @@
-const familyTree = {};
 const legend = ['name', 'age', 'residence', 'occupancy', 'hobbies', 'spouse', 'parents', 'siblings', 'children'];
 
-const person = ['Eric', 'Sarah', 'Ann', 'Tommy', 'Jim', 'Kelly', 'Mary', 'Nicolas'];
+var person = ['Eric', 'Sarah', 'Ann', 'Tommy', 'Jim', 'Kelly', 'Mary', 'Nicolas'];
 
-const Eric = ['Eric', 30, 'London', 'Programmer', ['Morning running', 'Fresh juice'], 'Sarah', [, 'Mary'], , ['Ann']];
-const Sarah = ['Sarah', 26, 'London', 'Layer', ['Watch series', 'Write articles to locals newspapers'], 'Eric', ['Jim', 'Kelly'], , ['Ann']];
-const Ann = ['Ann', 4, 'London', , ['Crayon drawing', 'Playing with Tommy'], '', ['Eric', 'Sarah'], ,];
-const Tommy = ['Tommy', 4, 'London'];
-const Jim = ['Jim', 65, 'Manchester', 'Retired', ['Travel England'], 'Kelly', , , ['Sarah']];
-const Kelly = ['Kelly', 65, 'Manchester', 'Retired', ['Travel England'], 'Jim', , , ['Sarah']];
-const Mary = ['Mary', 66, 'Liverpool', , , 'Nicolas', , , ['Eric']];
-const Nicolas = ['Nicolas', 70, 'Liverpool', 'Head of security', ['Prima violin in Liverpool orchestra'], 'Mary'];
+var Eric = ['Eric', 30, 'London', 'Programmer', ['Morning running', 'Fresh juice'], 'Sarah', [, 'Mary'], , ['Ann']];
+var Sarah = ['Sarah', 26, 'London', 'Layer', ['Watch series', 'Write articles to locals newspapers'], 'Eric', ['Jim', 'Kelly'], , ['Ann']];
+var Ann = ['Ann', 4, 'London', , ['Crayon drawing', 'Playing with Tommy'], '', ['Eric', 'Sarah'], ,];
+var Tommy = ['Tommy', 4, 'London'];
+var Jim = ['Jim', 65, 'Manchester', 'Retired', ['Travel England'], 'Kelly', , , ['Sarah']];
+var Kelly = ['Kelly', 65, 'Manchester', 'Retired', ['Travel England'], 'Jim', , , ['Sarah']];
+var Mary = ['Mary', 66, 'Liverpool', , , 'Nicolas', , , ['Eric']];
+var Nicolas = ['Nicolas', 70, 'Liverpool', 'Head of security', ['Prima violin in Liverpool orchestra'], 'Mary'];
 
+const familyTree = person.reduce((obj, key) => ({...obj, [key]: {}}), {});
 
-person.forEach(elem => addPersonlegend(elem, eval(elem)));
+person.forEach(elem => addPersonlegend(elem));
 person.forEach(elem => ['spouse', 'parents', 'children', 'sibllins'].forEach(elem1 => addFamilyTies(elem, elem1)));
 
 
-
-function addPersonlegend(person, characteristics) {
-  familyTree[person] = {};
+function addPersonlegend(person) {
+  const characteristics = this[person];
   for (let i = 0; i < legend.length; i++) {
-    familyTree[person][legend[i]] = characteristics[i] || 'unknown';
+    characteristics[i] && (familyTree[person][legend[i]] = characteristics[i]);
   }
 }
 
 function addFamilyTies(firstName, key) {
   let relatives = familyTree[firstName][key];
-  if (relatives !== undefined && relatives !== 'unknown') {
+  if (relatives !== undefined) {
     if (Array.isArray(relatives)) {
       relatives = relatives.map(e => e !== undefined ? familyTree[e] : undefined);
       familyTree[firstName][key] = relatives;
