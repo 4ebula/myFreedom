d482d37859efedc78ec8067138ff774a94df00df
4ebula <4ebula@gmail.com>
2021-06-02 09:24:33 +0300

    feat: add lessons

---
+++ lesson-22/lesson-22.js
@@ -0,0 +1,9 @@
+document.forms.thisform.thisname.focus();
+
+// focus() / blur()
+// Events: 'focus', 'blur';
+// focuson / focusout
+// onfocus / onblur
+// Event: 'change'
+// Event: 'input'
+// Event: 'cut', 'copy', 'paste'
